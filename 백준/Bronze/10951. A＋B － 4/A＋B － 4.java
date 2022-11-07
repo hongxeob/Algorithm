b226d9fb61dfa1fa957e3925abef2cc8d4ad90e8
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-11-07 22:56:58 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+        
+
+    }
+}
+
+
