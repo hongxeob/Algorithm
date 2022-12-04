a6e489c343eb528b90a97d0b84dc405f6c4d7ea3
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-04 21:45:40 +0900

    [level 0] Title: 외계행성의 나이, Time: 8.59 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String old = String.valueOf(age);
+        String[] arr = old.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += (char) ((Integer.parseInt(arr[i]) + 97)); // 소문자 a(여기서는 0)의 정수 아스키코드는 97이다
+        }
+        return answer;
+    }
+}
