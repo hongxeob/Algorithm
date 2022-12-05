7b7945bf35f43488088964529ea212982724c348
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-05 20:26:41 +0900

    [level 0] Title: 가위 바위 보, Time: 2.88 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,12 +1,14 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-        for (int i = 0; i < rsp.length(); i++) {
-            answer += (rsp.split("")[i].equals("0") ? "5"
-                    : rsp.split("")[i].equals("2") ? "0"
-                    : rsp.split("")[i].equals("5") ? "2" : "1");
+        String[] arr = rsp.split("");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("2")) {
+                answer += "0";
+            } else if (arr[i].equals("0")) {
+                answer += "5";
+            } else answer += "2";
         }
-
         return answer;
     }
 }
