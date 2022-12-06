f778a6d73b5aef335de6de8c03acad9912553e75
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-06 17:30:55 +0900

    [level 0] Title: 모스부호 (1), Time: 1.77 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public String solution(String letter) {
+        Map<String, String> alphabetMap = new HashMap<>() {
+            {
+                put(".-", "a");
+                put("-...", "b");
+                put("-.-.", "c");
+                put("-..", "d");
+                put(".", "e");
+                put("..-.", "f");
+                put("--.", "g");
+                put("....", "h");
+                put("..", "i");
+                put(".---", "j");
+                put("-.-", "k");
+                put(".-..", "l");
+                put("--", "m");
+                put("-.", "n");
+                put("---", "o");
+                put(".--.", "p");
+                put("--.-", "q");
+                put(".-.", "r");
+                put("...", "s");
+                put("-", "t");
+                put("..-", "u");
+                put("...-", "v");
+                put(".--", "w");
+                put("-..-", "x");
+                put("-.--", "y");
+                put("--..", "z");
+            }
+        };
+        String answer = "";
+        String[] arr = letter.split(" ");
+        for (String s : arr) {
+            answer += alphabetMap.get(s);
+        }
+        return answer;
+    }
+}
