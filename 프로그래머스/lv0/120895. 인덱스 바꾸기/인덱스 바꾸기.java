488d39a9f2c0abd7ffa6d10476e14df427ed9275
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-09 14:56:00 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.31 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == num1) {
+                answer = answer + arr[num2];
+            } else if (i == num2) {
+                answer = answer + arr[num1];
+            } else {
+                answer += arr[i];
+            } 
+        }
+        return answer;
+    }
+}
