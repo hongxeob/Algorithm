3a59b0108178273dc31552760e057ebcaa7f3a58
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-13 14:52:20 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 6.80 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+
+        return answer;
+    }
+}
