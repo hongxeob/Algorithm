77baa2dd582ef9405539d7a0428d93ffaaeb4ccb
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-13 15:01:55 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.58 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,8 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        int[] answer = Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
-
+        my_string = my_string.replaceAll("[a-z]", "");
+        int[] answer = new int[my_string.length()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i]=my_string.charAt(i)-'0'; // '0'은 아스키코드 48이기 때문에 빼준다, 이 과정이 없으면 안된다
+        }
+        Arrays.sort(answer);
         return answer;
     }
 }
