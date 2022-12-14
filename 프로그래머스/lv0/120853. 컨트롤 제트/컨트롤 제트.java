77cab87b980dedae1a9f7b0f4dee41a2789e6f6f
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-14 15:08:44 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(s.split(" ")));
+        while (arr.contains("Z")) {
+            arr.remove(arr.indexOf("Z") - 1);
+            arr.remove(arr.indexOf("Z"));
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            answer += Integer.parseInt(arr.get(i));
+        }
+        return answer;
+    }
+}
