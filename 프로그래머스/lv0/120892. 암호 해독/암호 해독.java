1aed1982a673310c1c93612fc91aca7adc04aeaf
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-16 16:06:11 +0900

    [level 0] Title: 암호 해독, Time: 2.80 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        String[] arr = cipher.split("");
+        for (int i = 0; i < arr.length; i++) {
+            if ((i + 1) % code == 0) {
+                answer += arr[i];
+                
+            }
+        }
+        return answer;
+    }
+}
