940ab4586e7be5611213a107d2ede4985d38a9e1
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-17 14:33:36 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.split(" ");
+        answer = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (i % 2 != 0) {
+                if (arr[i].equals("+")) {
+                    answer = answer + Integer.parseInt(arr[i + 1]);
+                } else {
+                    answer = answer - Integer.parseInt(arr[i + 1]);
+                }
+            }
+        }
+        return answer;
+    }
+}
