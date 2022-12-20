1d991d651d92ffa28827f5797199b50b6efdcbea
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-20 21:45:24 +0900

    [level 0] Title: 다항식 더하기, Time: 9.23 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -2,33 +2,33 @@ class Solution {
     public String solution(String polynomial) {
         String answer = "";
         String[] arr = polynomial.split(" +");
-        int linear = 0;
-        int constant = 0;
+        int beforeXNum = 0;
+        int onlyNum = 0;
         for (String s : arr) {
-            if (s.equals("x")) { // 잘라서 for문 돌리다가 x 그자체이면 
-                linear++; // x앞에 붙여줄 변수 증가
-            } else if (s.contains("x")) { // x가 2이상의 값이면(x가 숫자를 포함하고 있으면)
-                linear += Integer.parseInt(s.substring(0, s.length() - 1)); // 숫자만 잘라낸다. 
+            if (s.equals("x")) {
+                beforeXNum++;
+            } else if (s.contains("x")) {
+                beforeXNum += Integer.parseInt(s.substring(0, s.length() - 1));
             } else if (!s.equals("+")) {
-                constant += Integer.parseInt(s); // x랑 같지도,x를 포함하지도,부호도 아니고, 스트링형 숫자이면 고정값 증가
+                onlyNum += Integer.parseInt(s);
             }
         }
-        if (linear != 0 && constant == 0) { //x앞에 붙는 숫자가 있고, only숫자가 0이면
-            if (linear == 1) { //x가 1이면, 즉 1x 이면
-                answer += "x"; //x만 출력
+        if (beforeXNum != 0 && onlyNum == 0) {
+            if (beforeXNum == 1) {
+                answer += "x";
             } else {
-                answer += linear + "x"; //2이상 이면 ex)2x
-            }
+                answer += beforeXNum + "x";
+            } 
         }
-        if (linear != 0 && constant != 0) { // 다항식의 합(ex:2x+1,x+1)
-            if (linear == 1) { //ex: x+1
-                answer += "x" + " + " + constant;
-            } else { // ex : 2x+1
-                answer += linear + "x" + " + " + constant;
-            }
+        if (beforeXNum != 0 && onlyNum != 0) {
+            if (beforeXNum == 1) {
+                answer += "x" + " + " + onlyNum;
+            } else {
+                answer += beforeXNum + "x" + " + " + onlyNum;
+            } 
         }
-        if (linear == 0 && constant != 0) { // x는 없고 정수만
-            answer += constant; 
+        if (beforeXNum == 0 && onlyNum != 0) {
+            answer += onlyNum;
         }
         return answer;
     }
