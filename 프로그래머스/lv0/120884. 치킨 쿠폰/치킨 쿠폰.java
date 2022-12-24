f8a5beaf8b8e31d24deac49051227473c91c0e10
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-24 13:52:34 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,12 @@
 class Solution {
-    final int BONUS_NUM = 10;
-
-    public int solution(int chicken) { // 1081
-        int answer = 0;
-        while (chicken >= BONUS_NUM) {
-            int couponAndChicken = chicken / BONUS_NUM; // 108 // 10 // 1 // 1 
-            int restCoupon = chicken % BONUS_NUM; // 1 //9 //9//0
-            chicken = couponAndChicken + restCoupon; // 109 // 19 // 10//1
-            answer += couponAndChicken;//108+10+1+1
+    public int solution(int chicken) {
+        int coupon = chicken;
+        int service = 0;
+        while (coupon >= 10) { 
+            service += coupon / 10; 
+            coupon = coupon % 10 + coupon / 10;
         }
-        return answer;
+
+        return service;
     }
 }
