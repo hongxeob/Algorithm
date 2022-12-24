b9eb712ccf09ae06154d1bb4c936998d3feb2354
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2022-12-24 12:48:02 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,24 +1,14 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
-        String[] id_pw2 = new String[2];
         for (int i = 0; i < db.length; i++) {
-            if (id_pw[0].equals(db[i][0])) {
-                if (id_pw[1].equals(db[i][1])) {
-                    answer = "login";
-                } else {
-                    if (answer.contains("login")) {
-                        continue;
-                    } else {
-                        answer = "wrong pw";
-                    }
+            if (db[i][0].equals(id_pw[0])) {
+                if (db[i][1].equals(id_pw[1])) {
+                    return "login";
                 }
-            } else {
-                if (!answer.equals("login") && !answer.equals("wrong pw")) {
-                    answer = "fail";
-                }
+                return "wrong pw";
             }
         }
-        return answer;
+        return "fail";
     }
 }
