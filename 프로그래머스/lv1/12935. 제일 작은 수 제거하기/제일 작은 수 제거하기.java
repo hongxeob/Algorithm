85b0facc69dd015bca3344cd2764b969c3ef465b
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-01-06 21:55:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.10 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -4,18 +4,17 @@ class Solution {
             int[] answer = {-1};
             return answer;
         }
-        int[] answer = new int[arr.length - 1];
         int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            min = Math.min(arr[i], min);
+            min = Math.min(min, arr[i]);
         }
+        int[] answer = new int[arr.length - 1];
         int index = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == min) {
                 continue;
             }
             answer[index++] = arr[i];
-
         }
         return answer;
     }
