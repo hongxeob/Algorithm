a3f38b32d164728f0805d51116a50744149bc3be
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-01-09 20:08:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.17 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,8 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        String a = "수";
-        String b = "박";
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                answer += b;
-            } else {
-                answer += a;
-            }
+        for (int i = 0; i < n; i++) {
+            answer += i % 2 == 0 ? "수" : "박";
         }
         return answer;
     }
