5dc9e2152ddd61042db15136acab477b10291192
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-01-12 20:34:41 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.31 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+public String solution(String s) {
+        String answer = "";
+        int idx = 0;
+        String[] split = s.split("");
+        for (int i = 0; i < split.length; i++) {
+            if ((" ").equals(split[i])) {
+                idx = 0;
+            } else {
+                if (idx % 2 == 0) {
+                    idx++;
+                    split[i] = split[i].toUpperCase();
+                } else {
+                    idx++;
+                    split[i] = split[i].toLowerCase();
+                }
+            }
+            answer += split[i];
+        }
+        return answer;
+    }
+}
