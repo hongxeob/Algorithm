41b7da47da599a11c3263cf43d582df388b07047
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-02-01 12:18:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.66 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
         //중복이 있으면 안되므로 중복 허용이 안되는 HashSet 사용 (but 순서 보장 X)
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new TreeSet<>();
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 set.add(numbers[i] + numbers[j]);
             }
         }
         //순서를 보장하기 위해 List로 변환
-        ArrayList<Integer> arrayList = new ArrayList<>(set);
-        //오름차순
-        Collections.sort(arrayList);
+        List<Integer> arrayList = new ArrayList<>(set);
+        // //오름차순
+        // Collections.sort(arrayList);
         // int형 배열에 저장 하는 과정
         int[] answer = new int[arrayList.size()];
         for (int i = 0; i < arrayList.size(); i++) {
