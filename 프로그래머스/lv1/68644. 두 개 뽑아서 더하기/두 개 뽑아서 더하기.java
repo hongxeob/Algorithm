fc7719f8053f68f57a245dc72aff819ad06292e9
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-02-01 12:21:46 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.87 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        //중복이 있으면 안되므로 중복 허용이 안되는 HashSet 사용 (but 순서 보장 X)
+        //중복이 있으면 안되므로 중복 허용이 안되는 HashSet 사용 (but 순서 보장 X) -> TreeSet 쓰면 정렬까지 됨!
         Set<Integer> set = new TreeSet<>();
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
@@ -11,12 +11,12 @@ class Solution {
         }
         //순서를 보장하기 위해 List로 변환
         List<Integer> arrayList = new ArrayList<>(set);
-        // //오름차순
-        // Collections.sort(arrayList);
+        //오름차순
+//      Collections.sort(arrayList); -> hashSet 쓰면 정렬 해줘야 하지만 현재는 TreeSet(정렬도 되는)
         // int형 배열에 저장 하는 과정
         int[] answer = new int[arrayList.size()];
         for (int i = 0; i < arrayList.size(); i++) {
-            answer[i] = arrayList.get(i); //hashSet은 get() 메소드 존재 X -> set은 순서와 상관없이 중복되지 않는 것을 저장하는게 목적이므로
+            answer[i] = arrayList.get(i); //hashSet은 get() 메소드 존재 X -> set은 순서와 상관없이 중복되지 않는 것을 저장하는게 목적익므로
         }
         return answer;
     }
