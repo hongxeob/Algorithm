c72dd0780b270c751afad3a705f74f8a892d94d5
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-02-02 14:40:25 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.52 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,25 +2,12 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
-            String result = "";
-            String a1 = Integer.toBinaryString(arr1[i]);
-            String a2 = Integer.toBinaryString(arr2[i]);
-            for (int j = 0; j < n; j++) {
-                while (a1.length() < n) {
-                    a1 = "0" + a1;
-                }
-                while (a2.length() < n) {
-                    a2 = "0" + a2;
-                }
-                if (a1.charAt(j) == '1' || a2.charAt(j) == '1') {
-                    result += "#";
-                } else if (a1.charAt(j) == '0' && a2.charAt(j) == '0') {
-                    result += " ";
-                }
-                answer[i] = result;
-            }
+            String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            str = String.format("%" + n + "s", str);
+            str = str.replaceAll("1", "#");
+            str = str.replaceAll("0", " ");
+            answer[i] = str;
         }
-
         return answer;
     }
 }
