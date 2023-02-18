3d34688907b29e35bc2e781e892a2ad6a2d69f44
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-02-18 13:46:24 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -3,8 +3,11 @@ class Solution {
         int[] answer = new int[2];
         int zero = 0;
         int win = 0;
+        //먼저 맞춘 번호와 알아볼 수 없는 번호의 개수를 저장할 변수를 선언한다
+        //반복문으로 구매한 번호와 당첨번호를 비교해 win 카운트를 증가시킨다. 
+        //만약 구매한 로또번호가 알아볼 수 없는 번호라면 zero 카운트를 증가 시킨다
         for (int lotto : lottos) {
-            if (lotto == 0) {
+            if (lotto == 0) { // 번호를 알아볼 수 없는 경우
                 zero++;
             } else {
                 for (int win_num : win_nums) {
@@ -14,8 +17,8 @@ class Solution {
                 }
             } 
         }
-        answer[0] = win + zero > 1 ? 7 - (win + zero) : 6;
-        answer[1]= win > 1 ? 7 - win : 6;
+        answer[0] = (win+zero) > 1 ? 7-(win+zero) : 6; // 최고 순위
+        answer[1] = win > 1 ? 7-win : 6;               // 최저 순위
         return answer;
     }
 }
