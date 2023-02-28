6dde29fb556679b6a107bfc25020e44ae2cda432
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-02-28 11:46:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,9 +1,9 @@
 class Solution {
     public long solution(long n) {
-        Double sqrt = Math.sqrt(n);
-        if (sqrt == sqrt.intValue()) {
-            return (long) Math.pow(sqrt + 1, 2);
-        }
-        return -1;
+        if (Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        } else {
+            return -1;
+        } 
     }
 }
