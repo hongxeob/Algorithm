112c578e9b1bfc557355bc391cd20a50023a1169
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-01 15:57:53 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.47 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 1.33 ms
+메모리: 80.8 MB, 시간: 1.47 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Collections;
+
 class Solution {
     public long solution(long n) {
         String[] arr = Long.toString(n).split("");
@@ -8,9 +9,7 @@ class Solution {
         for (String s : arr) {
             str += s;
         }
-
-
+        
         return Long.parseLong(str);
-
     }
 }
