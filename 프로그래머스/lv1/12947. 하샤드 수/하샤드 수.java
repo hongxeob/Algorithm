e563c489d27e7862d1425800c1c533345979b163
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-02 11:18:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,15 +1,17 @@
 class Solution {
-    public boolean solution(int x) { //13
-        int sum = 0;
+    public boolean solution(int x) {
+        boolean answer = true;
+        int cnt = 0;
         int temp = x;
-        while (temp >= 1) {
-            sum += temp % 10;
+        while (temp > 0) {
+            cnt += temp % 10;
             temp /= 10;
         }
-        if (x % sum == 0) {
-            return true;
+        if (x % cnt == 0) {
+            answer = true;
+        } else {
+            answer = false;
         }
-        return false;
-    
+        return answer;
     }
 }
