ecbf7a8143841bfd4577458e4ccc2116ea15e797
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-05 20:16:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,8 +1,12 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for (int i = 0; i < signs.length; i++) {
-            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] == true) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            } 
         }
         return answer;
     }
