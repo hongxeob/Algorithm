5df70552701979b59d44f371200799a8558db020
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-08 14:00:11 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,16 +2,17 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         for (int i = left; i <= right; i++) {
-            int sum = 0;
+            int cnt = 0;
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0) {
-                    sum++;
+                    cnt++;
                 }
             }
-            if (sum % 2 == 0) {
+            if (cnt % 2 == 0) {
                 answer += i;
-            } else
+            } else {
                 answer -= i;
+            }
         }
         return answer;
     }
