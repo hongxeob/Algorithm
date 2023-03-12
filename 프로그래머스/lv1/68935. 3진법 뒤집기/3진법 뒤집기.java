c74efdf2212f71af660ddc90ca715040c1fa82d0
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-12 14:13:26 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.48 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,10 @@
 class Solution {
-   public int solution(int n) {
-        int answer = 0;
+    public int solution(int n) {
         String st = "";
         while (n != 0) {
             st += n % 3;
             n /= 3;
         }
-        return Integer.parseInt(st,3);
+        return Integer.parseInt(st, 3);
     }
 }
