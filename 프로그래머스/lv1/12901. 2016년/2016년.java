50762a711c02220a5adf304868f9c70173d2965c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-18 12:54:58 +0900

    [level 1] Title: 2016년, Time: 1.50 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,14 +1,8 @@
 import java.time.LocalDate;
+
 class Solution {
     public String solution(int a, int b) {
-        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        int date = 0;
-        for (int i = 0; i < a - 1; i++) {
-            date += month[i];
-        }
-        date += b - 1;
-        return days[date % 7];
+        
+        return LocalDate.of(2016,a,b).getDayOfWeek().toString().substring(0,3);
     }
 }
