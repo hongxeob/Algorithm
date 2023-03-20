949178d792f3173912341d558bdc8e0f6c828615
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-20 11:57:08 +0900

    [level 1] Title: 모의고사, Time: 0.57 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -3,26 +3,24 @@ import java.util.List;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1, 2, 3, 4, 5,}; //5개 반복 
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};//8개 반복
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10개 반복
+        int[] answer = {};
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
         int[] score = {0, 0, 0};
-
-        //수포자들의 맞힌 문제 갯수 계산
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i % 5]) score[0]++;
-            if (answers[i] == second[i % 8]) score[1]++;
-            if (answers[i] == third[i % 10]) score[2]++;
+            if (answers[i] == a[i % 5]) score[0]++;
+            if (answers[i] == b[i % 8]) score[1]++;
+            if (answers[i] == c[i % 10]) score[2]++;
         }
-        //최대 점수 구하기
         int max = Math.max(score[0], Math.max(score[1], score[2]));
-        
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < score.length; i++) {
-            if (max == score[i]) list.add(i + 1);
+            if (max == score[i]) {
+                list.add(i + 1);
+            }
         }
-
-        int[] answer = new int[list.size()];
+        answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
