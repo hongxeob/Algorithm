f4e1539bafbb22e7d255e35425b26576e926164a
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-21 12:08:38 +0900

    [unrated] Title: 과일 장수, Time: 0.36 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-
         Arrays.sort(score);
-
-        for(int i = score.length; i >= m; i -= m){
+        for (int i = score.length; i >= m; i -= m) {
             answer += score[i - m] * m;
         }
-
         return answer;
     }
 }
