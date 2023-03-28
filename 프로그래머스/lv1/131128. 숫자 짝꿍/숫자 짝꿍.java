62a29d8846db65a80da4fdb1cddbb10e58987da9
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-03-28 11:42:23 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.29 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,17 +1,15 @@
 class Solution {
-    public String solution(String X, String Y) {
+   public String solution(String X, String Y) {
         StringBuilder sb = new StringBuilder();
-        int[] xArr = new int[10]; // X 숫자별 개수 파악용
-        int[] yArr = new int[10]; // Y 숫자별 개수 파악용
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
 
-        //숫자별로 배열 SET
         for (String x : X.split("")) {
             xArr[Integer.parseInt(x)]++;
         }
         for (String y : Y.split("")) {
             yArr[Integer.parseInt(y)]++;
         }
-        //마지막(9)부터 반복하면서 둘 다 있는 숫자면 StringBuilder에 append!
         for (int i = 9; i >= 0; i--) {
             while (xArr[i] > 0 && yArr[i] > 0) {
                 sb.append(i);
