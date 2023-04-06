d2c751740f3181bf71232ed2632d9e95e0137094
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-06 11:50:21 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -1,20 +1,16 @@
 class Solution {
-    public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "Yes";
+     public String solution(String[] cards1, String[] cards2, String[] goal) {
         int card1Index = 0;
         int card2Index = 0;
         for (int i = 0; i < goal.length; i++) {
-            if (card1Index < cards1.length && goal[i].equals(cards1[card1Index])) {
-                card1Index++;
-                continue;
+            if (cards1[card1Index].equals(goal[i])) {
+                card1Index = (card1Index < cards1.length - 1) ? card1Index + 1 : card1Index;
+            } else if (cards2[card2Index].equals(goal[i])) {
+                card2Index = (card2Index < cards2.length - 1) ? card2Index + 1 : card2Index;
+            } else {
+                return "No";
             }
-            if (card2Index < cards2.length && goal[i].equals(cards2[card2Index])) {
-                card2Index++;
-                continue;
-            }
-            answer = "No";
-            return answer;
         }
-        return answer;
+        return "Yes";
     }
 }
