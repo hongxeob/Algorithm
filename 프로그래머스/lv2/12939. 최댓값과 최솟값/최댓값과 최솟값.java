55845e47a05becd269c2fdaebe6efccf2ae36ddc
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-06 22:42:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.83 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,19 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
-        //문자열 s를 공백 단위로 쪼갠다
-        String[] arr = s.split(" ");
-        int min = Integer.MAX_VALUE; // 223232323...
-        int max = Integer.MIN_VALUE; // -23232323....
-        //쪼갠것을 parseint해서 최대 최소 비교
-        //단 공백인 경우는 넘어간다.
-        for (int i = 0; i < arr.length; i++) {
-            int number = Integer.parseInt(arr[i]);
-            max = Math.max(number, max);
-            min = Math.min(number, min);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        String[] split = s.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            arr.add(Integer.parseInt(split[i]));
         }
-        answer = min + " " + max;
-        //최대 최소를 공백을 기준으로 리턴
+        answer = "" + Collections.min(arr) + " " + Collections.max(arr);
         return answer;
     }
 }
