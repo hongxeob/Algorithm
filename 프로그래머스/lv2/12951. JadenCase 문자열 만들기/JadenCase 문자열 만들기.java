40e3ee7e51fb410db4dcc0c11be93561a1de9bb1
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-11 20:48:02 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.40 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.toLowerCase().split("");
+        boolean space = true;
+        for (String ss : arr) {
+            answer += space ? ss.toUpperCase() : ss;
+            space = ss.equals(" ") ? true : false;
+        }
+        return answer;
+    }
+}
