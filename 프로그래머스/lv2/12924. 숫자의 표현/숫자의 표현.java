407b6ad3dab870eceae3c515657dc468d0091b48
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-14 11:43:35 +0900

    [level 2] Title: 숫자의 표현, Time: 0.50 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int cnt = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+        while (end_index != n) {
+            if (sum == n) {
+                cnt++;
+                end_index++;
+                sum += end_index;
+            } else if (sum > n) {
+                sum = sum - start_index;
+                start_index++;
+            } else {
+                end_index++;
+                sum += end_index;
+            } 
+        }
+        return cnt;
+    }
+}
