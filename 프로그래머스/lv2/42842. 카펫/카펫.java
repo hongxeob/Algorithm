c2855952aa5f6531fae9abcad5ab17ff02c54284
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-16 18:25:33 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -2,18 +2,19 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int sum = brown + yellow;
-        for (int i = 1; i <= sum; i++) {
-            int height = i; //세로
-            int width = sum / height; // 가로
-            if (height> width) {
-                continue;
-            }
-            if ((height - 2) * (width - 2) == yellow) {
-                answer[0] = width;
-                answer[1] = height;
-                break;
+
+        for (int i = 3; i < sum; i++) {
+            if (sum % i == 0) {
+                int width = sum / i;
+                if (width >= i) {
+                    if ((i - 2) * (width - 2) == yellow) {
+                        answer[0] = width;
+                        answer[1] = i;
+                    }
+                }
             }
         }
+
         return answer;
     }
 }
