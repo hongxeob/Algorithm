9cc28f93cee284ce2b8c57f606b0523e0d45c5dd
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-18 15:22:31 +0900

    Time: 4 ms (23.20%), Space: 43.3 MB (56.42%) - LeetHub

--- LeetCode/0724-find-pivot-index/0724-find-pivot-index.java
+++ LeetCode/0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,16 +1,15 @@
 class Solution {
-       public int pivotIndex(int[] nums) { //[1,7,3,6,5,6]
-        int sum = 0; //28
+    public int pivotIndex(int[] nums) {
+        int totalSum=Arrays.stream(nums).sum();
         int leftSum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int rightSum = sum - leftSum - nums[i];
-            if (rightSum == leftSum) {
-                return i;
+        int pivot = 0;
+        for(int i=0; i<nums.length; i++){
+            pivot = i;
+            totalSum-=nums[i];
+            if(totalSum==leftSum){
+                return pivot;
             }
-            leftSum += nums[i];
+            leftSum+=nums[i];
         }
         return -1;
     }
