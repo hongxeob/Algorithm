13fada6c4058ee33bce7e41c26ec8bcfa855fdbf
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-19 14:15:55 +0900

    Time: 1 ms (84.55%), Space: 40.2 MB (93.92%) - LeetHub

--- LeetCode/0392-is-subsequence/0392-is-subsequence.java
+++ LeetCode/0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,21 @@
 class Solution {
-public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) return true;
-
-        String[] arr = t.split("");
-        int index = 0, count = 0;
-        String sub = String.valueOf(s.charAt(index++));
-
-        for (int i = 0; i < t.length(); i++) {
-            if (sub.equals(arr[i])) {
-                if (index == s.length()) {
-                    count++;
+    public boolean isSubsequence(String s, String t) {
+       int prev = 0;
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i);
+            for (int j = prev; j < t.length(); j++) {
+                if (sc == t.charAt(j)) {
+                    cnt++;
+                    prev = j + 1;
                     break;
                 }
-                sub = String.valueOf(s.charAt(index++));
-                count++;
             }
         }
-
-        if (count == s.length()) return true;
-        else return false;
+        if (cnt == s.length()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
