53d426ad831203a5cd14568aa187ad5218b45f9a
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-20 21:31:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 352 ms, Memory: 22312 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,29 +2,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    static int checkArr[];
-    static int myArr[];
-    static int checkSecret;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1 - i; j++) {
-                if (A[j] > A[j + 1]) {
-                    int temp = A[j];
-                    A[j] = A[j + 1];
-                    A[j + 1] = temp;
+
+        bubbleSortCode(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    private static void bubbleSortCode(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) { //회차
+            for (int j = 0; j < arr.length - 1 - i; j++) {// 회차안의 인접한 숫자 비교
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-        for (int i = 0; i < N; i++) {
-            System.out.println(A[i]);
-        }
     }
-    
 }
