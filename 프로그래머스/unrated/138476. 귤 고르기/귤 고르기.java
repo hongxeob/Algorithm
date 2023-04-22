b388ecf27f158184fdb81ec62a360eb773af5eeb
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-22 14:22:41 +0900

    [unrated] Title: 귤 고르기, Time: 56.39 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> sizeMap = new HashMap<Integer, Integer>(); //사이즈,갯수
+        for (int size : tangerine) {
+            sizeMap.put(size, sizeMap.getOrDefault(size, 0) + 1);
+        }
+        List<Integer> keyList = new ArrayList<>(sizeMap.keySet());
+        keyList.sort((o1, o2) -> sizeMap.get(o2) - sizeMap.get(o1));
+        int i = 0;
+        while (k > 0) {
+            k -= sizeMap.get(keyList.get(i));
+            answer++;
+            i++;
+        }
+        return answer;
+    }
+}
