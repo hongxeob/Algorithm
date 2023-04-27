35efc52c39856d04d0a7c5bb21c896a41f23fd54
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-27 13:56:51 +0900

    [level 1] Title: 폰켓몬, Time: 4.38 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,14 @@
 import java.util.HashSet;
 
 class Solution {
-    public int solution(int[] nums) {
-        int pickNum = nums.length / 2;
+    public int solution(int[] nums) { // 3,1,2,3
+       int half = nums.length / 2;
         int answer = 0;
         HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]); // 3,1,2
         }
-        if (pickNum < set.size()) answer = pickNum;
+        if(half<set.size()) answer = half;
         else answer = set.size();
         return answer;
     }
