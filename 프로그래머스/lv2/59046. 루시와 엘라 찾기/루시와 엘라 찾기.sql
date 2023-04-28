7e599786ea12d0db1e9751b2f88a7e13a345ca5e
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-04-28 14:06:21 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
+SELECT ANIMAL_ID,	NAME,	SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+WHERE name = 'Lucy' or name = 'Ella' or name='Pickle' or name='Rogan'or name= 'Sabrina' or name= 'Mitty'
