dc1503f5793b163672422d3c549476bb782fb2a7
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-03 14:35:07 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toLowerCase().replace("a", "A");
+        return myString;
+    }
+}
