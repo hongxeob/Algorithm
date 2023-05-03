9cf9907eb7e31723af360923a07c7b3bf038af41
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-03 21:16:11 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (7.18%) - LeetHub

--- LeetCode/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ LeetCode/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] answer = new int[nums.length];
-        // answer[0]=nums[0];
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for(int i= 0; i<nums.length; i++){
             int temp = nums[i];
-            sum += temp;
-            answer[i] = sum;
+            sum+=temp;
+            answer[i]=sum;
         }
         return answer;
     }
