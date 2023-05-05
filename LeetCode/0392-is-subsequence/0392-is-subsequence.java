9b41f791d350c49062ed1e40e516447ed71a4335
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-05 15:08:44 +0900

    Time: 1 ms (85.17%), Space: 40.9 MB (23.97%) - LeetHub

--- LeetCode/0392-is-subsequence/0392-is-subsequence.java
+++ LeetCode/0392-is-subsequence/0392-is-subsequence.java
@@ -1,11 +1,10 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-       int prev = 0;
+public boolean isSubsequence(String s, String t) {
+        int prev = 0;
         int cnt = 0;
         for (int i = 0; i < s.length(); i++) {
-            char sc = s.charAt(i);
             for (int j = prev; j < t.length(); j++) {
-                if (sc == t.charAt(j)) {
+                if (s.charAt(i) == t.charAt(j)) {
                     cnt++;
                     prev = j + 1;
                     break;
@@ -17,5 +16,6 @@ class Solution {
         } else {
             return false;
         }
+
     }
 }
