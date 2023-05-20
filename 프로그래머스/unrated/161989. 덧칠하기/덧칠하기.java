97af93e72530ceb5e165bb1ee6dfaf0afdb73582
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-20 15:47:39 +0900

    [unrated] Title: 덧칠하기, Time: 0.83 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int solution(int n, int m, int[] section) {
+ public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int max = 0; // 빈곳 시작점 + 롤러 길이 
+        int max = 0;
         for (int i = 0; i < section.length; i++) {
             if (section[i] < max) {
-                continue; // 빈곳의 인덱스가 max의 범위 안에 있으면 패스(이미 칠해져 있다는 말)
+                continue;
             }
             answer += 1;
             max = section[i] + m;
