f839604c0002af3ae912f14b308d77fc2183b5a8
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-23 15:36:27 +0900

    [unrated] Title: 2의 영역, Time: 4.99 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int start = -1, end = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (start == -1) {
+                    start = i;
+                }
+                end = i;
+            }
+        }
+        if (start == -1) {
+            return new int[]{-1};
+        }
+        for (int i = start; i <= end; i++) {
+            list.add(arr[i]);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
