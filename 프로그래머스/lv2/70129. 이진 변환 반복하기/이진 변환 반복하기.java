eebea5131413b6712286c7e243ef8ca41f9ecd71
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-26 15:39:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.06 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] solution(String s) { // 110010101001
+    public int[] solution(String s) {
         int[] answer = new int[2];
         while (s.length() > 1) {
             int cntOne = 0;
@@ -8,7 +8,7 @@ class Solution {
                     answer[1]++;
                 } else {
                     cntOne++;
-                } 
+                }
             }
             s = Integer.toBinaryString(cntOne);
             answer[0]++;
