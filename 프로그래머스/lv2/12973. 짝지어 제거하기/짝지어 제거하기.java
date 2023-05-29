491b136ef3da44bd19fd6f9140ad771d8da34cb3
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-05-29 19:00:08 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.38 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,8 +1,8 @@
-import java.util.Stack;
-
+import java.util.*;
 class Solution
 {
     public int solution(String s) {
+        int answer = -1;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (!stack.isEmpty() && stack.peek() == c) {
