75d12dbca824c9e9320801d28228e092fc7e966d
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-05 16:42:38 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.59 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,20 +1,7 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int max = 0;
-        int index = 0;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (max < numbers[i]) {
-                max = numbers[i];
-                index = i;
-            }
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            if (i != index && answer < max * numbers[i]) {
-                answer = max * numbers[i];
-            }
-        }
-        return answer;
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
