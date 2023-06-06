df9d93b597998634988de2494117b3069a6c6634
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-06 14:12:42 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,9 +1,10 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = {};
         answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
-        
+
         return answer;
     }
 }
