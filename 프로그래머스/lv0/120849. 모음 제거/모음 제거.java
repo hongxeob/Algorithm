fe2f82b598b71185ddb76093fa2f17157c9b71b9
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-07 12:07:57 +0900

    [level 0] Title: 모음 제거, Time: 2.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,6 +1,14 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replaceAll("[aeiou]", "");
+        String answer = "";
+        String[] arr = my_string.split("");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("a") || arr[i].equals("e") || arr[i].equals("i") || arr[i].equals("o") || arr[i].equals("u")) {
+                continue;
+            } else {
+                answer += arr[i];
+            } 
+        }
         return answer;
     }
 }
