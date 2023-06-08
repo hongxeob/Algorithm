6007cf9f3b274e12aba2e001d486394b3e3023e5
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-08 23:02:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuffer str = new StringBuffer(my_string);
-        String answer = String.valueOf(str.reverse());// 문자형으로 바꿔준다
-        return answer;
+        StringBuffer sb = new StringBuffer(my_string);
+        return sb.reverse().toString();
     }
 }
