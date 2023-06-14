2e70cde36418072f5f1038f6f44afa9fd5ab2a37
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-14 19:18:59 +0900

    [unrated] Title: 정수 찾기, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -3,13 +3,9 @@ class Solution {
         int answer = 0;
         for(int i=0; i<num_list.length; i++){
             if(num_list[i]==n){
-                answer=1;
-                break;
-            }else{
-                answer=0;
-            }        
-    
+                return 1;
+            }
         }
-        return answer;
+        return 0;
     }
 }
