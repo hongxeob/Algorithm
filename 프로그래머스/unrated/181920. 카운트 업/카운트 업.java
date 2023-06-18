d2dc696e40a030385431dfbc3bfbc2512a5faf56
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-18 21:09:11 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[end-start+1];
-        for(int i=0; i<answer.length; i++){
-            answer[i]=start;
-            start++;           
+        int[] answer = new int[end - start + 1];
+        int idx = 0;
+        for (int i = start; i <= end; i++) {
+            answer[idx++] += i;
         }
         return answer;
     }
