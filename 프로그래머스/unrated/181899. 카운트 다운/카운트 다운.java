e64a9cf21e8f761621119a44a7e71e56120d475f
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-18 22:55:30 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] solution(int start, int end) {
         int[] answer = new int[start-end+1];
-        for(int i=0; i<=start-end; i++) {
-            answer[i] = start - i;
+        int idx =0 ;
+        for(int i=start; i>=end; i--){
+            answer[idx++]=i;
         }
         return answer;
     }
