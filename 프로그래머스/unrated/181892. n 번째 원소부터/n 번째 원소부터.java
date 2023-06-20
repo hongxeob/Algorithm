14de9c8a8defb778591f7c371fdc980b7ed88b98
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-20 19:05:55 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length-n +1];
-        int idx = 0;
-       for(int i = n-1; i<num_list.length; i++){
-           answer[idx++]=num_list[i];
-       }
+        List<Integer> list = new ArrayList<>();
+        for (int i = n - 1; i < num_list.length; i++) {
+            list.add(num_list[i]);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+            
+        }
         return answer;
     }
 }
