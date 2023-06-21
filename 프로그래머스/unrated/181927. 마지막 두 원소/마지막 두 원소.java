32de3a3dbb7ca3cb929ec395ec339d45c7d5ed3f
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-21 11:24:31 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
-       public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
+    public int[] solution(int[] num_list) {
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < num_list.length; i++) {
-            answer[i] = num_list[i];
+                list.add(num_list[i]);
+            }
+        
+        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+            list.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
+        } else {
+            list.add(num_list[num_list.length - 1] * 2);
         }
-        answer[num_list.length] = num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length - 1] - num_list[num_list.length - 2] : num_list[num_list.length - 1] * 2;
-
-
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
