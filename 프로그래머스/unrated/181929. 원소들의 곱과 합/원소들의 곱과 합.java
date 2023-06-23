8078fec1cb274ce6edda99506c5f010d188d3d5d
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-23 14:56:50 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,13 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int gob=1;
-        int ze=0;
+        int plus = 0;
+        int gob =1;
         for(int i=0; i<num_list.length; i++){
+            plus+= num_list[i];
             gob *= num_list[i];
-            ze +=num_list[i];
         }
-        
-        return gob<(ze*ze)?1:0;
+        if(plus*plus < gob){
+            answer = 0;
+        }else{
+            answer = 1;
+        }
+        return answer;
     }
 }
