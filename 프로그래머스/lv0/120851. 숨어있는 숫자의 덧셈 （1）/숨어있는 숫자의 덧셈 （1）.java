721746f8786757ef8eded0e42ffa6370e845696d
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-24 14:24:28 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.19 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(String my_string) {
-        int answer = 0;
-        String numbs = my_string.toLowerCase().replaceAll("[a-z]", "");
-        for (int i = 0; i < numbs.length(); i++) {
-            answer += Integer.parseInt(numbs.substring(i, i + 1));
-        }
+        my_string = my_string.replaceAll("[^0-9]", "");
+
+            int answer = 0;
+
+            for(int i=0; i<my_string.length(); i++) {
+                answer += Integer.parseInt(String.valueOf(my_string.charAt(i)));
+            }           
         return answer;
     }
 }
