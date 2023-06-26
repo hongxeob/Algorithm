c63f93a3552d80f4894f231a0b578e5f0e7be0bd
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-26 11:16:37 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,6 +1,16 @@
+import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
-      return my_string.split(" ");
+        List<String> list = new ArrayList<>();
+        String[] arr = my_string.split(" ");
         
+        for(int i=0; i<arr.length; i++){
+            list.add(arr[i]);
+        }
+        String[] answer = new String[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i]=list.get(i);
+        }
+        return answer;
     }
 }
