c1a4359e2e8eaf99f971fce47c0653f8270bda4b
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-06-27 16:37:44 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 0.04 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i]; j++) {
                 list.add(arr[i]);
             }
         }
         int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
+        for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
         return answer;
