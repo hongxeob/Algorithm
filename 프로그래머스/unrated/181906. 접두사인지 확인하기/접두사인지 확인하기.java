dfb901b20e706de8e6f0347908bb07376dcfe941
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-01 17:57:49 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,6 +1,9 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        
-return my_string.startsWith(is_prefix) ? 1 : 0;    }
+        if(my_string.startsWith(is_prefix)){
+            answer =1;
+        }
+        return answer;
+    }
 }
