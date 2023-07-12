39ab33e7ad53f8fa2ba887a456270f2668be37f9
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-12 22:02:11 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-        for (int i = 1; i <= t; i++) {
-            n *= 2;
-            answer = n;
-        }
+        int answer = n* (int) Math.pow(2, t);
+        
         return answer;
     }
 }
