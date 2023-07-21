465f3caa5dfa286eac83908d8c7ad3ffac72bfdf
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-21 14:48:07 +0900

    Time: 1 ms (86.58%), Space: 44.6 MB (53.11%) - LeetHub

--- LeetCode/0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ LeetCode/0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -18,21 +18,19 @@ class Node {
 */
 
 class Solution {
+    public List<Integer> res = new ArrayList<>();
+    
     public List<Integer> preorder(Node root) {
-     List<Integer> list = new ArrayList<>();  
-        if(root == null) return list;
+      dfs(root);
+        return res;
         
-        Stack<Node> stack = new Stack<>();
-        stack.add(root);
-        
-        while(!stack.empty()){
-            root = stack.pop();
-            list.add(root.val);
-            for(int i=root.children.size()-1; i>=0; i--){
-                stack.add(root.children.get(i));
-            }
-            
-        }
-        return list;
+    }
+    
+    public void dfs(Node root){
+      if(root == null) return;
+      res.add(root.val);
+      for(Node node : root.children){
+          dfs(node);
+      }    
     }
 }
