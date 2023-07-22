8ea630e47b083b63c958b19df2e1584886eee678
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-22 17:32:35 +0900

    Time: 0 ms (100.00%), Space: 45.3 MB (13.43%) - LeetHub

--- LeetCode/0704-binary-search/0704-binary-search.java
+++ LeetCode/0704-binary-search/0704-binary-search.java
@@ -1,16 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left =0 ;
-        int right = nums.length -1;
-        while(right>=left){
-            int mid = (left+right) /2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]<target){
-                left = mid +1;
-            }else{
-                right = mid -1 ;
-            }
+        int start = 0;
+        int end = nums.length - 1;
+
+    
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+          if(nums[mid]==target){
+              return mid;
+          }else if(target > nums[mid]){
+              start = mid+1;
+          }else{
+              end = mid -1;
+          }
         }
         return -1;
     }
