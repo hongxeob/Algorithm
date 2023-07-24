95e04bff831633d543c5fff94feb49ec5048e87c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-24 11:56:07 +0900

    Time: 0 ms (100.00%), Space: 44.1 MB (56.84%) - LeetHub

---
+++ LeetCode/0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) {
+            return image;
+        }
+
+        int startColor = image[sr][sc];
+
+        fill(image, sr, sc, startColor, newColor);
+        return image;
+    }
+
+    public void fill(int[][] image, int sr, int sc, int startColor, int newColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length ||
+                sc >= image[0].length || image[sr][sc] != startColor) {
+            return;
+        }
+
+        image[sr][sc] = newColor;
+
+        fill(image, sr - 1, sc, startColor, newColor);
+        fill(image, sr + 1, sc, startColor, newColor);
+        fill(image, sr, sc - 1, startColor, newColor);
+        fill(image, sr, sc + 1, startColor, newColor);
+    }
+}
