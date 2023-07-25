bc51a77e559cc13373b022f5237df5dd5931b6cf
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-25 11:02:20 +0900

    [Bronze II] Title: 피보나치 수, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static int[] D;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        D = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            D[i] = -1;
+        }
+        D[0] = 0;
+        D[1] = 1;
+        fibo(n);
+        System.out.println(D[n]);
+    }
+
+    static int fibo(int n) {
+        if (D[n] != -1) {
+            return D[n];
+        }
+        return D[n] = fibo(n - 2) + fibo(n - 1);
+    }
+}
