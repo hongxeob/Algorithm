154cb44a167cc6fa802d92d03d840a436aeef355
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-29 14:15:21 +0900

    Time: 3 ms (34.27%), Space: 40.8 MB (56.11%) - LeetHub

---
+++ LeetCode/0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack1.isEmpty() && c == '#') {
+                stack1.pop();
+            } else {
+                if (c != '#') {
+                    stack1.push(c);
+                }
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (!stack2.isEmpty() && c == '#') {
+                stack2.pop();
+            } else {
+                if (c != '#') {
+                    stack2.push(c);
+                }
+            }
+        }
+        return stack1.equals(stack2);
+    }
+}
