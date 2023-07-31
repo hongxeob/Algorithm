1cda0a759508dcb9a660dba8d8190bc63c40191f
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-31 15:13:19 +0900

    [level 2] Title: 더 맵게, Time: 1840.18 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i : scoville) {
+            pq.add(i);
+        }
+
+        while (pq.peek() < K) {
+            if (pq.size() == 1) {
+                return -1;
+            }
+            pq.add(pq.poll() + pq.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+}
