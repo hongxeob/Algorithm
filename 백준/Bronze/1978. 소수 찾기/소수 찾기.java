1317fe852c97b62fea27e3fe98156259eba640fd
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-07-31 14:08:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 216 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            boolean isPrime = true;
+            int num = sc.nextInt();
+
+            if (num == 1) {
+                continue;
+            }
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
