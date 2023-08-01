6fa21777305360ff53d3767800b237520e67869f
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-01 21:56:44 +0900

    [Bronze II] Title: 커트라인, Time: 268 ms, Memory: 20708 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[n - k]);
+
+    }
+}
