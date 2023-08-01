c6003d8d238772e708a94e0a905384600f1c0a68
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-01 22:14:14 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 784 ms, Memory: 57620 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -16,23 +16,22 @@ public class Main {
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][1] = Integer.parseInt(st.nextToken());
             arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
+            if (o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
             } else {
-                return o1[0] - o2[0];
+                return Integer.compare(o1[1], o2[1]);
             }
         });
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
         }
         System.out.println(sb);
     }
-
 }
