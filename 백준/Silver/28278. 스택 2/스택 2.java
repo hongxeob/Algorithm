874cfa6661b1172487dd2e4fea506fb442f2eae0
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-02 21:50:11 +0900

    [Silver IV] Title: 스택 2, Time: 2256 ms, Memory: 340384 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int select = sc.nextInt();
+            if (select == 1) {
+                int num = sc.nextInt();
+                stack.push(num);
+            } else if (select == 2) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append('\n');
+                } else {
+                    sb.append(-1).append('\n');
+                }
+            } else if (select == 3) {
+                sb.append(stack.size()).append('\n');
+            } else if (select == 4) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append('\n');
+                } else {
+                    sb.append(0).append('\n');
+                }
+            } else {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek()).append('\n');
+                } else {
+                    sb.append(-1).append('\n');
+                }
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
