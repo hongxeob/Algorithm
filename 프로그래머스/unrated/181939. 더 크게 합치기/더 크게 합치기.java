a567d6f86c44ff06c95315e958ce573710d432d4
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-22 21:35:48 +0900

    [unrated] Title: 더 크게 합치기, Time: 8.66 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,17 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        String a1 = String.valueOf(a);
-        String a2 = String.valueOf(b);
-        
-        return Integer.parseInt(a1+a2) >= Integer.parseInt(a2+a1) ? Integer.parseInt(a1+a2):Integer.parseInt(a2+a1) ;
+
+        if (a == b) {
+            return Integer.parseInt(a + "" + b);
+        }
+        int i = Integer.parseInt(a + "" + b);
+        int j = Integer.parseInt(b + "" + a);
+
+        if (i > j) {
+            return i;
+        } else {
+            return j;
+        }
     }
 }
