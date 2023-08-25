efaac8c4a5413e25abc7fa6cfa844ae408d6f6f2
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-25 12:36:26 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,18 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
-     public String[] solution(String[] names) {
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < names.length; i++) {
-            if (i % 5 == 0) {
-                list.add(names[i]);
-            }
+    public String[] solution(String[] names) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < names.length; i += 5) {
+            list.add(names[i]);
         }
+
         String[] answer = new String[list.size()];
+
         for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
         return answer;
     }
-
-
 }
