fa057f250ee358bcc8ce13f401cd9154db9c937c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-29 21:16:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,13 +1,10 @@
 class Solution {
-   public int solution(int n) {
-        int answer = 0;
-        Double sqrt = Math.sqrt(n); // n값의 제곱근을 sqrt에 담아준다. Math.sqrt의 반환타입은 double형 이므로 변수 타입은 double로 선언.
+    public int solution(int n) {
 
-        if (sqrt == sqrt.intValue()) { // 실수형인 sqrt 값을 intValue메소드에 넣어 정수값으로 반환
-            answer = 1; // 같으면(제곱근이면) 1 
-        } else { // 제곱근이 아니면 2
-            answer = 2;
+        if (Math.sqrt(n) % 1 == 0) {
+            return 1;
+        } else {
+            return 2;
         }
-        return answer;
     }
 }
