2700244edb46607d8f5ef03cfe478dca660702c8
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-08-31 20:39:08 +0900

    [unrated] Title: l로 만들기, Time: 12.11 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.charAt(i) <= 'l') {
-                answer += "l";
-            } else {
-                answer += myString.charAt(i) + "";
-            } 
-        }
+        String answer = myString.replaceAll("[a-k]", "l");
         return answer;
     }
 }
