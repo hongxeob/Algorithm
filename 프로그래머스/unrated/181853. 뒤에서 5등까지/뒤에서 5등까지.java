4f973aee73f5c882e96a8bfb49da64e8af8ec770
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-09-02 21:57:40 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.41 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,11 +1,13 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
-        Arrays.sort(num_list);
-        for (int i = 0; i < 5; i++) {
-            answer[i] = num_list[i];
-        }
-        return answer;
-    }
+	public int[] solution(int[] num_list) {
+		int[] answer = new int[5];
+		Arrays.sort(num_list);
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = num_list[i];
+		}
+		return answer;
+	}
 }
