c692b62a155e418534e6276c6ffc25928e79f99b
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-09-03 18:36:26 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -1,18 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
-     public String[] solution(String[] todo_list, boolean[] finished) {
-        
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < todo_list.length; i++) {
-            if (!finished[i]) {
-                list.add(todo_list[i]);
-            }
-        }
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
-    }
+	public String[] solution(String[] todo_list, boolean[] finished) {
+		List<String> list = new ArrayList<>();
+
+		for (int i = 0; i < todo_list.length; i++) {
+			if (!finished[i]) {
+				list.add(todo_list[i]);
+			}
+		}
+		String[] answer = new String[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
 }
