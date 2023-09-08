76a2577b85bc64eb768d10f19e9933e556587880
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-09-08 21:20:53 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,17 +1,21 @@
+import java.util.*;
+
 class Solution {
- public int[] solution(int n, int[] numlist) {
-        int[] arr = new int[numlist.length]; // 배수인지 확인할 배열 만들고
-        int cnt = 0; // 숫자들을 담을 변수
-        for (int i = 0; i < numlist.length; i++) {
-            if (numlist[i] % n == 0) { // [i]번째 숫자가 n으로 나누어지면
-                arr[cnt] = numlist[i]; // numlist[i]번째 수를 담는다. cnt의 그 자리에 그숫자를 담음 
-                cnt++; // 담긴 그 숫자 다음 인덱스로 증가한다 (인덱스 한칸 이동한다고 보면 될듯?!)
+    public int[] solution(int n, int[] numlist) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<numlist.length; i++){
+            if(numlist[i]%n==0){
+                list.add(numlist[i]);
             }
         }
-        int[] answer = new int[cnt]; // 정답은 cnt의 배열을 가진다.
-        for (int i = 0; i < cnt; i++) { // cnt 만큼 for문을 돌면서 
-            answer[i] = arr[i]; // answer[i] 각 자리에 넣어준다
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            answer[i]=list.get(i);
         }
+        
         return answer;
     }
 }
