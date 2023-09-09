b2c3452605dce7096b5407c34de1c22050e127eb
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-09-09 19:58:21 +0900

    [level 0] Title: 대문자와 소문자, Time: 3.84 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,13 +1,13 @@
 class Solution {
-    public String solution(String my_string) { // cCcCcC
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            if (Character.isUpperCase(my_string.charAt(i))) {
-                answer += Character.toLowerCase(my_string.charAt(i));
-            } else {
-                answer += Character.toUpperCase(my_string.charAt(i));
-            } 
-        }
+    public String solution(String my_string) {
+       String answer="";
+        for(int i=0; i<my_string.length(); i++) {
+                if(Character.isUpperCase(my_string.charAt(i))) {                //i번째 문자가 대문자면 
+                    answer+=String.valueOf(my_string.charAt(i)).toLowerCase();  //소문자로 바꿔서 더함
+                }else {                                                         //i번째 문자가 소문자면
+                    answer+=String.valueOf(my_string.charAt(i)).toUpperCase();  //대문자로 바꿔서 더함
+                }
+            }
         return answer;
     }
 }
