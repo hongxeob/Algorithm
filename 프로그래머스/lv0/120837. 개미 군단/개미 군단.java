a122015e348a78cdf692c684baaaaf6259186223
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-10-01 13:26:38 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,13 +1,6 @@
 class Solution {
-    public int solution(int hp) {
-        int answer = 0;
-        answer += hp / 5;
-        hp = hp % 5;
+	public int solution(int hp) {
 
-        answer += hp / 3;
-        hp = hp % 3;
-
-        answer += hp / 1;
-        return answer;
-    }
+		return (hp / 5) + ((hp % 5) / 3) + ((hp % 5) % 3);
+	}
 }
