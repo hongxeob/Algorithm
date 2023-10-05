b14330d5e56717522d22942dc641a89fcd5ed98c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-10-05 15:32:45 +0900

    [level 0] Title: 숫자 찾기, Time: 0.10 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,11 +1,16 @@
 class Solution {
-   public int solution(int num, int k) {
-        String numS = Integer.toString(num); // 정수를 String화
-        String sK = Integer.toString(k); // 정수를 String화
-        if (numS.indexOf(sK) == -1) { // numS에 sK가 없으면 -1
-            return -1;
-        } else {
-            return numS.indexOf(sK) + 1; // 있으면 그 자릿수 반환.근데 0부터 시작하니 +1
-        }
-    }
+	public int solution(int num, int k) {
+		int answer = 0;
+		String[] arr = String.valueOf(num).split("");
+		for (int i = 0; i < arr.length; i++) {
+			if (Integer.parseInt(arr[i]) == k) {
+				answer = i + 1;
+				break;
+			}else{
+				answer = -1;
+			}
+		}
+
+		return answer;
+	}
 }
