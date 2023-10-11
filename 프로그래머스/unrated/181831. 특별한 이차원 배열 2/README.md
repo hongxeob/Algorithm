a2ee82c2ca23059eb0e5aa3915cd722a3a0da5c2
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-10-11 12:53:23 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/README.md
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/README.md
@@ -1,10 +1,10 @@
-# [unrated] 특별한 이차원 배열 2 - 181831 
+# [level unrated] 특별한 이차원 배열 2 - 181831 
 
 [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181831) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 0.01 ms
+메모리: 64.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -12,7 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2023년 10월 3일 12:53:19
 
 ### 문제 설명
 
@@ -133,4 +137,4 @@ Empty
 </ul>
 
 
-> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,13 +1,13 @@
 class Solution {
-        public int solution(int[][] arr) {
+    public int solution(int[][] arr) {
         int answer = 1;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    answer = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i][j]!=arr[j][i]){
+                    answer=0;
                 }
             }
-        }   
-        return answer;  
+        }
+        return answer;
     }
 }
