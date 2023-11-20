a19a62dc8a80f35e390f41ae4ea832cc8a5baa83
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2023-11-20 16:08:35 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.06 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,20 +1,23 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
-        public int[] solution(int n) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(n);
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n = (3 * n + 1);
-            }
-            list.add(n);
-        }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
-    }
+	public int[] solution(int n) {
+		List<Integer> list = new ArrayList<>();
+		list.add(n);
+		while (n != 1) {
+			if (n % 2 == 0) {
+				n = n / 2;
+				list.add(n);
+			} else {
+				n = 3 * n + 1;
+                list.add(n);
+			}
+		}
+		int[] answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
 }
