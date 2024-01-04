6fe9013ed7db2d9d5e4903eea169cdbd8709d974
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-04 12:56:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.76 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 25.44 ms
+메모리: 118 MB, 시간: 24.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 23:19:33
+2024년 1월 4일 12:56:53
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,20 +1,22 @@
 import java.util.*;
 
 public class Solution {
-    	public int[] solution(int[] arr) {
-		List<Integer> set = new ArrayList<>();
-		set.add(arr[0]);
+   public int[] solution(int[] arr) {
+
+		List<Integer> list = new ArrayList<>();
+		list.add(arr[0]);
+
 		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] != arr[i - 1]) {
-				set.add(arr[i]);
+				list.add(arr[i]);
 			}
 		}
+		int[] answer = new int[list.size()];
 
-		int[] answer = new int[set.size()];
-
-		for (int i = 0; i < set.size(); i++) {
-			answer[i] = set.get(i);
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
 		}
+
 		return answer;
 	}
 }
