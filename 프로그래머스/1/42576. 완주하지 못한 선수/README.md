35d2494030935b6a0f9d0e8c08617e1b26dacb49
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-22 16:14:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.58 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95 MB, 시간: 252.77 ms
+메모리: 95.4 MB, 시간: 82.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 14:56:32
+2024년 1월 1일 16:14:21
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,22 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
    	public String solution(String[] participant, String[] completion) {
 		String answer = "";
-		Arrays.sort(participant);
-		Arrays.sort(completion);
-
-		for (int i = 0; i < completion.length; i++) {
-			if (completion[i].equals(participant[i])) {
-				continue;
-			} else {
-				return participant[i];
+		HashMap<String, Integer> map = new HashMap<>();
+		for (String player : participant) {
+			map.put(player, map.getOrDefault(player, 0) + 1);
+		}
+		for (String player : completion) {
+			map.put(player, map.get(player) - 1);
+		}
+		for (String s : map.keySet()) {
+			if (map.get(s) != 0) {
+				answer = s;
+				break;
+				
 			}
 		}
-		return participant[participant.length - 1];
+		return answer;
 	}
 }
