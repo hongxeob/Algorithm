95e521a913c9792f02053d523697f145781aff9e
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-22 16:37:09 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,15 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String,Integer> map = new HashMap<>();
-        for(String[] cloth : clothes){
-            
-            map.put(cloth[1],map.getOrDefault(cloth[1],0)+1);
-        }
-        for(Integer i : map.values()){
-            answer*=i.intValue()+1;
-        }
-        return answer-1;
-    }
+		int answer = 1;
+		HashMap<String, Integer> map = new HashMap<>();
+
+		for (String[] clothe : clothes) {
+			String type = clothe[1];
+			map.put(type, map.getOrDefault(type, 0) + 1);
+		}
+		for (Integer value : map.values()) {
+			answer *= value + 1;
+		}
+		return answer - 1;
+	}
 }
