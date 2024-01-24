a16bbe4334e01e813f416b020d23954db8b83297
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-24 16:47:49 +0900

    [Bronze III] Title: 최소, 최대, Time: 592 ms, Memory: 90592 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,18 +1,28 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		int[] n = new int[t];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < t; i++) {
-			n[i] = sc.nextInt();
+		Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int max = -1000001;
+		int min = 1000001;
+
+		while (st.hasMoreTokens()) {
+			int value = Integer.parseInt(st.nextToken());
+			if (value > max) {
+				max = value;
+			}
+			if (value < min) {
+				min = value;
+			}
 		}
-
-		Arrays.sort(n);
-
-		System.out.println(n[0] + " " + n[t - 1]);
+		System.out.println(min + " " + max);
 	}
 }
