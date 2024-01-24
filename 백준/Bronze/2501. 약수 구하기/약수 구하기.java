2b5a65777f569f42fd25e1ef7e6bb82bf6105e6e
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-24 12:47:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,22 +1,22 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+	public static void main(String[] args) {
+		// 1.N,K 입력 받음
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            if (N % i == 0) {
-                K--;
-            }
-            if (K == 0) {
-                System.out.println(i);
-                return;
-            }
-        }
-        System.out.println(0);
-    }
+		List<Integer> list = new ArrayList<>();
+		// 2. N의 약수 구한다.
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) {
+				list.add(i);
+			}
+		}
+
+		// 3. K-1 번째 숫자 구하기.
+		System.out.println(list.size() < K ? 0 : list.get(K - 1));
+	}
 }
