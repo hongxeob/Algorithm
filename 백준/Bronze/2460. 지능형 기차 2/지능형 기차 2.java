21b208073a6814373d87b55502d73e49a255d364
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-25 17:05:16 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -10,19 +10,19 @@ public class Main {
 
 		int sum = 0;
 		int max = 0;
-
-		int[][] passenger = new int[10][2];
+		int human = 0;
 
 		for (int i = 0; i < 10; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			passenger[i][0] = Integer.parseInt(st.nextToken());
-			passenger[i][1] = Integer.parseInt(st.nextToken());
+			int out = Integer.parseInt(st.nextToken());
+			int in = Integer.parseInt(st.nextToken());
 
-			sum = sum - passenger[i][0] + passenger[i][1];
+			human -= out;
+			human += in;
 
-			if (max < sum) {
-				max = sum;
+			if (human > max) {
+				max = human;
 			}
 		}
 		System.out.println(max);
