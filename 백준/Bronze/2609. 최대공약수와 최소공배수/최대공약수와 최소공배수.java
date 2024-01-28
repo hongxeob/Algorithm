fbac9f598ecb33fb91e5b004610fd058b84763b6
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-28 14:15:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,16 +1,14 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-
-		System.out.println(gcd(num1, num2));
-		System.out.println(lcm(num1, num2));
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(gcd(a, b));
+		System.out.println(lcm(a, b));
 
 	}
 
