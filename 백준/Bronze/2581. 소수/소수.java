69410b25a75de49467cdb5b27688d279ba351ef0
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-01-28 14:29:34 +0900

    [Bronze II] Title: 소수, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,41 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int sum = 0;
-		List<Integer> list = new ArrayList<>();
-
+		int min = 0;
 		for (int i = a; i <= b; i++) {
-			boolean prime = false;
-			prime = isPrime(i);
-			if (prime) {
-				list.add(i);
+			if (isPrime(i)) {
 				sum += i;
+				if (min == 0) {
+					min = i;
+				}
 			}
 		}
-        
-		if (list.size() == 0) {
+		if (sum == 0) {
 			System.out.println(-1);
 		} else {
 			System.out.println(sum);
-			System.out.println(list.get(0));
+			System.out.println(min);
 		}
+
 	}
 
-	public static boolean isPrime(int num) {
-		if (num < 2) {
-			return false;
-		}
+	public static boolean isPrime(int n) {
+		if (n < 2) return false;
 
-		for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (num % i == 0) {
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
 				return false;
 			}
 		}
