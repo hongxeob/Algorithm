9db3fbcb12507c8632ff6213745ddf79441b06ac
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-01 18:13:56 +0900

    [Gold IV] Title: 부분합, Time: 872 ms, Memory: 109220 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,37 +1,39 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int S = sc.nextInt();
-		int[] arr = new int[N];
+
+		int n = sc.nextInt();
+		int s = sc.nextInt();
+		int[] arr = new int[n];
 
 		int start = 0;
 		int end = 0;
 		int len = Integer.MAX_VALUE;
 		int sum = 0;
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
 
 		while (true) {
-			if (sum >= S) {
+			if (sum >= s) {
 				sum -= arr[start++];
 				len = Math.min(len, (end - start) + 1);
-			} else if (end == N) {
+			} else if (end == n) {
 				break;
 			} else {
 				sum += arr[end++];
 			}
 		}
+
 		if (len == Integer.MAX_VALUE) {
 			System.out.println(0);
 		} else {
 			System.out.println(len);
 		}
 	}
-
 }
