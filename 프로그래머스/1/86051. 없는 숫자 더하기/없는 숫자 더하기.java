1af957e91225a7faabe135411af57ada9beb169c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-04 21:48:35 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        for (int number : numbers) {
+            sum -= number;
+        }
+        return sum;
+    }
+}
