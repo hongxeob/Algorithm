43a0f5d9d4a280de138e1937137902e1a47de957
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-09 15:37:28 +0900

    [level 1] Title: 둘만의 암호, Time: 0.22 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -4,21 +4,22 @@ import java.util.List;
 
 class Solution {
 public String solution(String s, String skip, int index) {
-		String[] temp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                          "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-                          "w", "x", "y", "z"};
-        List<String> alphabets = new ArrayList<>(Arrays.asList(temp));
-        
-        for(int i=0; i<skip.length(); i++){
-            alphabets.remove(String.valueOf(skip.charAt(i)));
-        } // 알파벳 삭제
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            int sIdx = alphabets.indexOf(String.valueOf(s.charAt(i)));
-            int nextSIdx = (sIdx+index)%alphabets.size();
-            sb.append(alphabets.get(nextSIdx));
-        }
-        return sb.substring(0);
-    }
+		String[] temp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+		List<String> alpha = new ArrayList<>(Arrays.asList(temp));
+
+		//skip 문자를 리스트에서 제거
+		for (int i = 0; i < skip.length(); i++) {
+			alpha.remove(String.valueOf(skip.charAt(i)));
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			//s를 잘라서 인덱스 만큼 더 해준다.
+			int sIdx = alpha.indexOf(String.valueOf(s.charAt(i)));
+			int nextIdx = (sIdx + index) % alpha.size();
+			sb.append(alpha.get(nextIdx));
+		}
+		return sb.toString();
+	}
 }
