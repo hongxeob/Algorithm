8f6a560280457df3713c999553b08e80d84ed9b4
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-12 14:34:01 +0900

    [level 1] Title: 둘만의 암호, Time: 0.15 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -5,20 +5,17 @@ import java.util.List;
 class Solution {
 public String solution(String s, String skip, int index) {
 		String[] temp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		List<String> list = new ArrayList<>(List.of(temp));
 
-		List<String> alpha = new ArrayList<>(Arrays.asList(temp));
-
-		//skip 문자를 리스트에서 제거
 		for (int i = 0; i < skip.length(); i++) {
-			alpha.remove(String.valueOf(skip.charAt(i)));
+			list.remove(String.valueOf(skip.charAt(i)));
 		}
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < s.length(); i++) {
-			//s를 잘라서 인덱스 만큼 더 해준다.
-			int sIdx = alpha.indexOf(String.valueOf(s.charAt(i)));
-			int nextIdx = (sIdx + index) % alpha.size();
-			sb.append(alpha.get(nextIdx));
+			int sIdx = list.indexOf(String.valueOf(s.charAt(i)));
+			int nextIdx = (sIdx + index) % list.size();
+			sb.append(list.get(nextIdx));
 		}
 		return sb.toString();
 	}
