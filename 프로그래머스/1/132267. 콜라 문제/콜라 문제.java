2b91094be00349eb97a5330a7d491060fa115de4
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-13 21:34:11 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+public int solution(int a, int b, int n) {
+		int answer = 0;
+		int temp = 0;
+		while (n >= a) {
+			if (n < a) {
+				break;
+			}
+			int givenMart = (n / a) * b;
+			answer += givenMart;
+			int remain = n % a;
+			n = givenMart + remain;
+		}
+		return answer;
+	}
+}
