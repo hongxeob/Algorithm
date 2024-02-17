76300ec548c76ad34c661868f43d559e0017886c
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-17 13:53:54 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 71.5 MB, 시간: 0.09 ms
+메모리: 71.9 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 11:28:35
+2024년 2월 6일 13:53:52
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,23 +1,25 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            if (i != 0) {
-                String s1 = words[i - 1]; // 이전 단어
-                String s2 = words[i]; // 현재 단어
-                char last = s1.charAt(s1.length() - 1);
-                char first = s2.charAt(0);
-                if (map.containsKey(s2) || last != first) {
-                    answer[0] = (i % n) + 1; //먼저 떨어진 번호
-                    answer[1] = (i / n) + 1; // 몇번째 차례에 떨어졌는지
-                    return answer;
-                }
-            }
-            map.put(words[i], 1);
-        }
+	public int[] solution(int n, String[] words) {
+		//가장 먼저 탈락하는 사람 번호, 그 사람이 자신의 몇번째 차례에 탈락
+		int[] answer = {0, 0};
 
-        return answer;
-    }
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < words.length; i++) {
+			if (i != 0) {
+				String prev = words[i - 1];
+				String now = words[i];
+				char end = prev.charAt(prev.length() - 1);
+				char start = now.charAt(0);
+				if (map.containsKey(now) || end != start) {
+					answer[0] = (i % n) + 1;
+					answer[1] = (i / n) + 1;
+
+					return answer;
+				}
+			}
+			map.put(words[i], i);
+		}
+		return answer;
+	}
 }
