4bf5570bc2176d722173e62936378573fae711b1
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-02-18 13:49:29 +0900

    [level 1] Title: 2016년, Time: 0.96 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,8 +1,7 @@
 import java.time.LocalDate;
 
 class Solution {
-    public String solution(int a, int b) {
-		String answer = LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
-		return answer;
+public String solution(int a, int b) {
+		   return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
 	}
 }
