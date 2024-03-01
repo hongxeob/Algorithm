1bae82b553b748b5292510b76f5775672da277fc
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-01 11:35:22 +0900

    [level 1] Title: 덧칠하기, Time: 1.06 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int solution(int n, int m, int[] section) {
-		int answer = 0;
-		int paintedBefore = 0;
+    	public int solution(int n, int m, int[] section) {
+		int answer = 1;
+		int start = section[0];
+		int end = section[0] + (m - 1);
 
-		for (int i = 0; i < section.length; i++) {
-			// 칠해야하는 부분이 칠해진 부분에 속한다면 (이미 칠해졌다면)
-			if (section[i] < paintedBefore) {
+		for (int i : section) {
+			if (i >= start && i <= end) {
 				continue;
+			} else {
+				start = i;
+				end = i + (m - 1);
+				answer++;
 			}
-			answer++;
-			// 다시 더 칠했다는 말이므로 filled는 현재 칠해야하는 섹션 + 롤러의 길이
-			// 즉 paintedBefore 해당 값 전까지 칠해져 있다. ex: 6이면 5까지 칠해져있음.
-			paintedBefore = section[i] + m;
+
 		}
 		return answer;
 	}
