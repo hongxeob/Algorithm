9352de3b3b1e6c813302e060e3c10b9fe520e9cc
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-01 11:38:58 +0900

    [level 1] Title: 모의고사, Time: 0.59 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int[] solution(int[] answers) {
+    	public int[] solution(int[] answers) {
 		int[] a = {1, 2, 3, 4, 5};
 		int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
 		int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
@@ -19,15 +19,16 @@ class Solution {
 				score[2]++;
 			}
 		}
-		int max = Math.max(Math.max(score[0], score[1]), score[2]);
-
 		List<Integer> list = new ArrayList<>();
+		int max = Math.max(Math.max(score[0], score[1]), score[2]);
+		
 		for (int i = 0; i < score.length; i++) {
-			if (max == score[i]) {
+			if (score[i] == max) {
 				list.add(i + 1);
 			}
 		}
 		int[] answer = new int[list.size()];
+
 		for (int i = 0; i < list.size(); i++) {
 			answer[i] = list.get(i);
 		}
