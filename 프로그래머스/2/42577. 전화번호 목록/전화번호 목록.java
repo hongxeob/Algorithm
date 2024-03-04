1dab942bae72139a3c3f59c7a737163a63589f15
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-04 17:38:35 +0900

    [level 2] Title: 전화번호 목록, Time: 199.98 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,16 @@
 import java.util.*;
+
 class Solution {
-    	public boolean solution(String[] phone_book) {
-		boolean answer = true;
-		Arrays.sort(phone_book);
-		for (int i = 0; i < phone_book.length-1; i++) {
-			if (phone_book[i + 1].startsWith(phone_book[i])) {
-				return false;
+    public boolean solution(String[] phone_book) {
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < phone_book.length; i++) {
+			map.put(phone_book[i], i);
+		}
+		for (int i = 0; i < phone_book.length; i++) {
+			for (int j = 1; j < phone_book[i].length(); j++) {
+				if (map.containsKey(phone_book[i].substring(0, j))) {
+					return false;
+				}
 			}
 		}
 		return true;
