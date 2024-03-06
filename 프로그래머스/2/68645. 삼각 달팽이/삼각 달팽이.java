a35042d5193a0472d41c86a33386fb1218a4993e
Hongseob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-06 20:25:28 +0900

    [level 2] Title: 삼각 달팽이, Time: 11.14 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,50 +1,48 @@
 class Solution {
 		public int[] solution(int n) {
-		int[][] tree = new int[n][n];
+		int[][] triangle = new int[n][n];
 		int cnt = 1;
 		int x = 0;
 		int y = 0;
 
 		while (true) {
-			//아래로 갈 때 
+
 			while (true) {
-				tree[y][x] = cnt++;
-				if (y + 1 == n || tree[y + 1][x] != 0) break;
+				triangle[y][x] = cnt++;
+				if (y + 1 == n || triangle[y + 1][x] != 0) break;
 				y += 1;
 			}
-			if (x + 1 == n || tree[y][x + 1] != 0) break;
+			if (x + 1 == n || triangle[y][x + 1] != 0) break;
 			x += 1;
 
-			//오른쪽으로 갈 때
 			while (true) {
-				tree[y][x] = cnt++;
-				if (x + 1 == n || tree[y][x + 1] != 0) break;
+				triangle[y][x] = cnt++;
+				if (x + 1 == n || triangle[y][x + 1] != 0) break;
 				x += 1;
 			}
-			if (tree[y - 1][x - 1] != 0) break;
+			if (triangle[y - 1][x - 1] != 0) break;
 			//왼쪽위로 가기 위해 한칸씩 빼준다
 			x -= 1;
 			y -= 1;
 
 			//왼쪽 위로 갈때
 			while (true) {
-				tree[y][x] = cnt++;
-				if (tree[y - 1][x - 1] != 0) break;
+				triangle[y][x] = cnt++;
+				if (triangle[y - 1][x - 1] != 0) break;
 				x -= 1;
 				y -= 1;
 			}
-			if (tree[y + 1][x] != 0 || y + 1 == n) break;
+			if (triangle[y + 1][x] != 0 || y + 1 == n) break;
             y += 1;
 		}
 
 		int[] answer = new int[cnt - 1];
 		int idx = 0;
-		for (int i = 0; i < tree.length; i++) {
+		for (int i = 0; i < triangle.length; i++) {
 			for (int j = 0; j <= i; j++) {
-				answer[idx++] = tree[i][j];
+				answer[idx++] = triangle[i][j];
 			}
 		}
-		
 		return answer;
 	}
 }
