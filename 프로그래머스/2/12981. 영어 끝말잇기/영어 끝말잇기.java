0a043d547a334a5babe25fb0d036edb2216cf812
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-20 22:36:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,25 +1,27 @@
 import java.util.*;
 class Solution {
-	public int[] solution(int n, String[] words) {
-		//가장 먼저 탈락하는 사람 번호, 그 사람이 자신의 몇번째 차례에 탈락
-		int[] answer = {0, 0};
-
+    	public int[] solution(int n, String[] words) {
+		int[] answer = new int[2];
 		Map<String, Integer> map = new HashMap<>();
+
 		for (int i = 0; i < words.length; i++) {
 			if (i != 0) {
 				String prev = words[i - 1];
 				String now = words[i];
+
 				char end = prev.charAt(prev.length() - 1);
 				char start = now.charAt(0);
-				if (map.containsKey(now) || end != start) {
+
+				if (map.containsKey(now) || start != end) {
 					answer[0] = (i % n) + 1;
 					answer[1] = (i / n) + 1;
-
-					return answer;
+                    return answer;
 				}
 			}
-			map.put(words[i], i);
+
+			map.put(words[i], 1);
 		}
+
 		return answer;
 	}
 }
