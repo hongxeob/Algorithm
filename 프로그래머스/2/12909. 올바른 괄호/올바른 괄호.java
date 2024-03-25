3cf535f4b303709f4f7f327dc23c7bd777c804d7
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-03-25 20:17:33 +0900

    [level 2] Title: 올바른 괄호, Time: 6.39 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,23 @@
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        int openCount = 0;
-        int closeCount = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                openCount++;
-            } else if (s.charAt(i) == ')') {
-                closeCount++;
-            }
-            if (openCount < closeCount) {
-                return false;
-            }
-        }
-        if (openCount == closeCount) {
-            return true;
-        }
-        return false;
-    }
+	boolean solution(String s) {
+		int openCount = 0;
+		int closeCount = 0;
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				openCount++;
+			} else if (c == ')') {
+				closeCount++;
+			}
+
+			if (openCount < closeCount) {
+				return false;
+			}
+		}
+
+		if (openCount == closeCount) {
+			return true;
+		}
+		return false;
+	}
 }
