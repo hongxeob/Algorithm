4a93dab074c03345a07090e5fb79c81545bd71a3
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-04-03 19:59:03 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,35 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-       		List<Integer> list = new ArrayList<>();
-		int[] worksDay = new int[progresses.length];
+		List<Integer> list = new ArrayList<>();
+		int[] works = new int[progresses.length];
 
 		for (int i = 0; i < speeds.length; i++) {
-			worksDay[i] = (100 - progresses[i]) / speeds[i]; // 7,3,9
+			works[i] = (100 - progresses[i]) / speeds[i];
 			if ((100 - progresses[i]) % speeds[i] != 0) {
-				worksDay[i] += 1;
+				works[i] += 1;
 			}
 		}
 
-		int prev = worksDay[0];
+		int prev = works[0];
 		int cnt = 1;
+
 		for (int i = 1; i < progresses.length; i++) {
-			if (prev >= worksDay[i]) {
-				cnt += 1;
+			if (prev >= works[i]) {
+				cnt++;
 			} else {
 				list.add(cnt);
 				cnt = 1;
-				prev = worksDay[i];
+				prev = works[i];
 			}
 		}
 		list.add(cnt);
-
 		int[] answer = new int[list.size()];
 
-		for (int i = 0; i < list.size(); i++) {
-			answer[i] = list.get(i);
-		}
-		return answer;
-	}
+        for (int i = 0; i <list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
 }
