77830ac43a4598671b887a32f8fb4f7bd2aba927
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-05-27 12:37:48 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.04 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(seoul: Array<String>): String {
         var answer = ""
-        for (i in 0..seoul.lastIndex) {
-            if (seoul[i] == "Kim") {
-                answer += "김서방은 " + i + "에 있다"
+        seoul.forEachIndexed { i, it ->
+            if (it == "Kim") {
+                answer = "김서방은 ${i}에 있다"
             }
         }
         return answer
