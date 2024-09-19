e3d14dbbf67ab42d2cc38442c577d048bf5f19a6
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-09-19 13:24:24 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,6 +1,7 @@
 def solution(num_list):
+    answer = -1
     for i in num_list:
         if i < 0:
-            return num_list.index(i)
-    
-    return -1
+             return num_list.index(i)
+             break
+    return answer 
