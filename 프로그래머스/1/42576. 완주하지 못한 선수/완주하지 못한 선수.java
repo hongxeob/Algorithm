ed4f38367adf6747628ab034089b2ac3248f5f2b
HongSeob Lee <97447334+hongxeob@users.noreply.github.com>
2024-10-31 16:52:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.13 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,23 @@
 import java.util.HashMap;
 
 class Solution {
-   	public String solution(String[] participant, String[] completion) {
-		String answer = "";
-		HashMap<String, Integer> map = new HashMap<>();
-		for (String player : participant) {
-			map.put(player, map.getOrDefault(player, 0) + 1);
-		}
-		for (String player : completion) {
-			map.put(player, map.get(player) - 1);
-		}
-		for (String s : map.keySet()) {
-			if (map.get(s) != 0) {
-				answer = s;
-				break;
-				
-			}
-		}
-		return answer;
-	}
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String player : participant){
+            map.put(player,map.getOrDefault(player,0)+1);
+        }
+        
+        for(String player : completion){
+            map.put(player,map.getOrDefault(player,0)-1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer=key;
+            }
+        }
+        
+        return answer;
+    }
 }
